bb3cf625d84421eddc0ac2c5b5c14b8b8131ad1a
hackerman103020 <94463996+hackerman103020@users.noreply.github.com>
2023-09-22 16:42:11 -0500

    Update usb_hid_autofire.c

--- usb_hid_autofire.c
+++ usb_hid_autofire.c
@@ -145,14 +145,7 @@ if (btn_right_autofire == false){
       }
     }
 
-      if(btn_left_autofire) {
-            furi_hal_hid_mouse_press(HID_MOUSE_BTN_LEFT);
-            // TODO: Don't wait, but use the timer directly to just don't send the release event (see furi_hal_cortex_delay_us)
-//furi_delay_us(1);
-            furi_hal_hid_mouse_release(HID_MOUSE_BTN_LEFT);
-        furi_hal_hid_mouse_press(HID_MOUSE_BTN_LEFT);
-        furi_hal_hid_mouse_release(HID_MOUSE_BTN_LEFT);
- //furi_delay_us(1);
+      lay_us(1);
         }
       if(btn_right_autofire) {
             furi_hal_hid_mouse_press(HID_MOUSE_BTN_RIGHT);
@@ -165,6 +158,15 @@ if (btn_right_autofire == false){
 
     view_port_update(view_port);
   }
+
+
+if(btn_left_autofire) {
+            furi_hal_hid_mouse_press(HID_MOUSE_BTN_LEFT);
+            // TODO: Don't wait, but use the timer directly to just don't send the release event (see furi_hal_cortex_delay_us)
+//furi_delay_us(1);
+            furi_hal_hid_mouse_release(HID_MOUSE_BTN_LEFT);
+ //furi_de
+
 
   furi_hal_usb_set_config(usb_mode_prev, NULL);
 
